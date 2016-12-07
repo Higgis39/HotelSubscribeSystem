25b8a4a0badcf3850438050d74c9024b51b4c081
Higgis39 <451413126@qq.com>
2016-12-07 16:30:46 +0800

    对Order对Promotion模块调用接口的修改

--- src/main/java/businessLogic/orderbl/stub/PromotionInfoForOrder.java
+++ src/main/java/businessLogic/orderbl/stub/PromotionInfoForOrder.java
@@ -4,10 +4,14 @@ import java.util.List;
 
 import po.OrderPO;
 import po.PromotionPO;
+import vo.HotelPromotionVO;
 import vo.OrderVO;
 import vo.PromotionVO;
+import vo.WebPromotionVO;
 
 public interface PromotionInfoForOrder {
 
-	public PromotionVO getAppropriatePromotion(OrderVO vo);
+	public WebPromotionVO getAppropriateWebPromotion(OrderVO vo);
+	
+	public HotelPromotionVO getApproriateHotelPromotion(OrderVO vo);
 }
--- src/main/java/businessLogic/orderbl/stub/PromotionInfoForOrder_stub.java
+++ src/main/java/businessLogic/orderbl/stub/PromotionInfoForOrder_stub.java
@@ -4,13 +4,22 @@ import java.util.List;
 
 import po.OrderPO;
 import po.PromotionPO;
+import vo.HotelPromotionVO;
 import vo.OrderVO;
 import vo.PromotionVO;
+import vo.WebPromotionVO;
 
 public class PromotionInfoForOrder_stub implements PromotionInfoForOrder{
 
+
 	@Override
-	public PromotionVO getAppropriatePromotion(OrderVO vo) {
+	public WebPromotionVO getAppropriateWebPromotion(OrderVO vo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public HotelPromotionVO getApproriateHotelPromotion(OrderVO vo) {
 		// TODO Auto-generated method stub
 		return null;
 	}
