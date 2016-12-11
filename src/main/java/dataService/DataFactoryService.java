5e3e5e116eec79349ecbcbb51b5fdd34c9355fab
Higgis39 <451413126@qq.com>
2016-12-11 20:07:25 +0800

    上传datafactory

--- src/main/java/dataService/DataFactoryService.java
+++ src/main/java/dataService/DataFactoryService.java
@@ -1,5 +1,20 @@
 package dataService;
 
-public class DataFactoryService {
+import java.rmi.Remote;
+
+public interface DataFactoryService extends Remote{
+	public HotelDataService getHotelDataService();
+	
+	public HotelPromotionDataService getHotelPromotionDataService();
+	
+	public OrderDataService getOrderDataService();
+	
+	public RoomDataService getRoomDataService();
+	
+	public UserDataService getUserDataService();
+	
+	public WebPromotionDataService getWebPromotionDataService();
+	
+	public WorkerDataService getWorkerDataService();
 
 }
