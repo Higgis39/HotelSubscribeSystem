136b9ec45eb8eec3fc07f93115dfcf247126b4c9
Thpffcj <1441732331@qq.com>
2016-12-23 22:25:09 +0800

    修改webpromotionbl

--- src/main/java/businessLogic/promotionbl/WebSpecificTimePromotion.java
+++ src/main/java/businessLogic/promotionbl/WebSpecificTimePromotion.java
@@ -10,7 +10,7 @@ import java.util.Date;
  */
 public class WebSpecificTimePromotion implements WebPromotionType{
 
-	public double calculateDiscount(String hotelId) {
+	public double calculateDiscount(String hotelId, String userId) {
 		
 		double discount = 0.8;
 		if(isInPromotionTime()){
